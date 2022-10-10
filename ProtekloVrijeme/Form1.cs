9aab3b1a15c4599025dad4e4d44c13f031901681
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-10-10 12:24:38 +0200

    Add files via upload

---
+++ ProtekloVrijeme/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace ProtekloVrijeme
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        DateTime pocetno = DateTime.Now;
+        private void timer_Tick(object sender, EventArgs e)
+        {
+           
+            toolStripStatusLabel2.Text = "Trenutno: " + DateTime.Now.ToLongTimeString();
+            TimeSpan proteklo = new TimeSpan();
+            proteklo = DateTime.Now - pocetno;
+            toolStripStatusLabel1.Text = "Proteklo: " + proteklo.Hours.ToString() + ":" + proteklo.Minutes.ToString() + ":" + proteklo.Seconds.ToString();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Tick += new EventHandler(timer_Tick); // Everytime timer ticks, timer_Tick will be called
+            timer.Interval = (1000) * (1);              // Timer will tick evert second
+            timer.Enabled = true;                       // Enable the timer
+            timer.Start();
+          
+      
+
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        } 
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+    }
+}
